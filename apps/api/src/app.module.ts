4d4a56bd6db9de7dd3ff6db3aa6766c3b9b204a9
Ricardo Alf <rear_recinos@hotmail.com>
2024-12-17 10:05:19 -0600

    feat: Add connection with postgress

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,17 +1,26 @@
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+
+import { EnvConfiguration, JoiValidationSchema, typeorm } from './config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { EnvConfiguration } from './config/env.config';
-import { JoiValidationSchema } from './config/joi.validation';
-
 @Module({
   imports: [
+    ConfigModule,
     ConfigModule.forRoot({
-      load: [EnvConfiguration],
+      load: [EnvConfiguration, typeorm],
       validationSchema: JoiValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        ...config.get('database'),
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
---
+++ apps/api/src/config/index.ts
@@ -0,0 +1,5 @@
+import typeorm, { databaseConnect } from './typeorm';
+
+export { typeorm, databaseConnect };
+export { EnvConfiguration } from './env.config';
+export { JoiValidationSchema } from './joi.validation';
